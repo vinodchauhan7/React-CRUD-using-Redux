aafcd330f55d7ee627e5fc3005d49c9bcfbd953f
vinodchauhan7 <vinodc45@gmail.com>
2019-08-03 19:36:20 +0530

    Adding Router to our app.
    In this PR, we have added router to our app and add welcomeComponent page with two buttons

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { BrowserRouter as Router } from "react-router-dom";
+import RootRouter from "./utils/rootRouter";
 import "./styles.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {/**
+    In our first lesson, we will add router to our app.
+    To add it, first of all add dependency on CLI using 
+    command "npm install react-router-dom --save".
+    */}
+      {/** This <Router> is used for adding routing in our app.
+      So it must added in root of our app.
+    */}
+      <Router>
+        <RootRouter />
+      </Router>
     </div>
   );
 }
